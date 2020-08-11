7d255d0cc1c6826f507bd032bfd6edac23bb8450
lubaijie <lubaijie_lbj@163.com>
2020-08-11 10:38:35 +0800

    dialog修改

--- packages/dialog/src/component.vue
+++ packages/dialog/src/component.vue
@@ -2,35 +2,43 @@
   <transition
     name="dialog-fade"
     @after-enter="afterEnter"
-    @after-leave="afterLeave">
+    @after-leave="afterLeave"
+  >
     <div
       v-show="visible"
       class="el-dialog__wrapper"
-      @click.self="handleWrapperClick">
+      @click.self="handleWrapperClick"
+    >
       <div
-        role="dialog"
         :key="key"
+        ref="dialog"
+        role="dialog"
         aria-modal="true"
         :aria-label="title || 'dialog'"
         :class="['el-dialog', { 'is-fullscreen': fullscreen, 'el-dialog--center': center }, customClass]"
-        ref="dialog"
-        :style="style">
-        <div class="el-dialog__header">
+        :style="style"
+      >
+        <div v-if="isTitle" class="el-dialog__header">
           <slot name="title">
             <span class="el-dialog__title">{{ title }}</span>
           </slot>
-          <button
-            type="button"
-            class="el-dialog__headerbtn"
-            aria-label="Close"
-            v-if="showClose"
-            @click="handleClose">
-            <i class="el-dialog__close el-icon el-icon-close"></i>
-          </button>
         </div>
-        <div class="el-dialog__body" v-if="rendered"><slot></slot></div>
-        <div class="el-dialog__footer" v-if="$slots.footer">
-          <slot name="footer"></slot>
+
+        <!-- 关闭按钮 -->
+        <button
+          v-if="showClose"
+          type="button"
+          class="el-dialog__headerbtn"
+          style="height: 30px; width: 30px; z-index: 9999"
+          aria-label="Close"
+          @click="handleClose"
+        >
+          <i class="el-dialog__close el-icon el-icon-close" />
+        </button>
+
+        <div v-if="rendered" class="el-dialog__body" :style="dialogBody"><slot /></div>
+        <div v-if="$slots.footer" class="el-dialog__footer">
+          <slot name="footer" />
         </div>
       </div>
     </div>
@@ -38,175 +46,189 @@
 </template>
 
 <script>
-  import Popup from 'element-ui/src/utils/popup';
-  import Migrating from 'element-ui/src/mixins/migrating';
-  import emitter from 'element-ui/src/mixins/emitter';
+import Popup from 'element-ui/src/utils/popup';
+import Migrating from 'element-ui/src/mixins/migrating';
+import emitter from 'element-ui/src/mixins/emitter';
 
-  export default {
-    name: 'ElDialog',
+export default {
+  name: 'YDialog',
 
-    mixins: [Popup, emitter, Migrating],
+  mixins: [Popup, emitter, Migrating],
 
-    props: {
-      title: {
-        type: String,
-        default: ''
-      },
-
-      modal: {
-        type: Boolean,
-        default: true
-      },
-
-      modalAppendToBody: {
-        type: Boolean,
-        default: true
-      },
-
-      appendToBody: {
-        type: Boolean,
-        default: false
-      },
-
-      lockScroll: {
-        type: Boolean,
-        default: true
-      },
-
-      closeOnClickModal: {
-        type: Boolean,
-        default: true
-      },
-
-      closeOnPressEscape: {
-        type: Boolean,
-        default: true
-      },
-
-      showClose: {
-        type: Boolean,
-        default: true
-      },
-
-      width: String,
-
-      fullscreen: Boolean,
-
-      customClass: {
-        type: String,
-        default: ''
-      },
-
-      top: {
-        type: String,
-        default: '15vh'
-      },
-      beforeClose: Function,
-      center: {
-        type: Boolean,
-        default: false
-      },
-
-      destroyOnClose: Boolean
+  props: {
+    title: {
+      type: String,
+      default: ''
     },
 
-    data() {
-      return {
-        closed: false,
-        key: 0
-      };
+    isTitle: {
+      type: Boolean,
+      default: true
     },
 
-    watch: {
-      visible(val) {
-        if (val) {
-          this.closed = false;
-          this.$emit('open');
-          this.$el.addEventListener('scroll', this.updatePopper);
-          this.$nextTick(() => {
-            this.$refs.dialog.scrollTop = 0;
-          });
-          if (this.appendToBody) {
-            document.body.appendChild(this.$el);
-          }
-        } else {
-          this.$el.removeEventListener('scroll', this.updatePopper);
-          if (!this.closed) this.$emit('close');
-          if (this.destroyOnClose) {
-            this.$nextTick(() => {
-              this.key++;
-            });
-          }
+    modal: {
+      type: Boolean,
+      default: true
+    },
+
+    modalAppendToBody: {
+      type: Boolean,
+      default: true
+    },
+
+    appendToBody: {
+      type: Boolean,
+      default: false
+    },
+
+    lockScroll: {
+      type: Boolean,
+      default: true
+    },
+
+    closeOnClickModal: {
+      type: Boolean,
+      default: true
+    },
+
+    closeOnPressEscape: {
+      type: Boolean,
+      default: true
+    },
+
+    showClose: {
+      type: Boolean,
+      default: true
+    },
+
+    // eslint-disable-next-line vue/require-default-prop
+    width: String,
+
+    fullscreen: Boolean,
+
+    customClass: {
+      type: String,
+      default: ''
+    },
+
+    top: {
+      type: String,
+      default: '15vh'
+    },
+    // eslint-disable-next-line vue/require-default-prop
+    beforeClose: Function,
+    center: {
+      type: Boolean,
+      default: false
+    },
+
+    destroyOnClose: Boolean,
+
+    dialogBody: {
+      type: Object,
+      default: () => {
+        return {};
+      }
+    }
+  },
+
+  data() {
+    return {
+      closed: false,
+      key: 0
+    };
+  },
+
+  computed: {
+    style() {
+      const style = {};
+      if (!this.fullscreen) {
+        style.marginTop = this.top;
+        if (this.width) {
+          style.width = this.width;
         }
       }
-    },
+      return style;
+    }
+  },
 
-    computed: {
-      style() {
-        let style = {};
-        if (!this.fullscreen) {
-          style.marginTop = this.top;
-          if (this.width) {
-            style.width = this.width;
-          }
-        }
-        return style;
-      }
-    },
-
-    methods: {
-      getMigratingConfig() {
-        return {
-          props: {
-            'size': 'size is removed.'
-          }
-        };
-      },
-      handleWrapperClick() {
-        if (!this.closeOnClickModal) return;
-        this.handleClose();
-      },
-      handleClose() {
-        if (typeof this.beforeClose === 'function') {
-          this.beforeClose(this.hide);
-        } else {
-          this.hide();
-        }
-      },
-      hide(cancel) {
-        if (cancel !== false) {
-          this.$emit('update:visible', false);
-          this.$emit('close');
-          this.closed = true;
-        }
-      },
-      updatePopper() {
-        this.broadcast('ElSelectDropdown', 'updatePopper');
-        this.broadcast('ElDropdownMenu', 'updatePopper');
-      },
-      afterEnter() {
-        this.$emit('opened');
-      },
-      afterLeave() {
-        this.$emit('closed');
-      }
-    },
-
-    mounted() {
-      if (this.visible) {
-        this.rendered = true;
-        this.open();
+  watch: {
+    visible(val) {
+      if (val) {
+        this.closed = false;
+        this.$emit('open');
+        this.$el.addEventListener('scroll', this.updatePopper);
+        this.$nextTick(() => {
+          this.$refs.dialog.scrollTop = 0;
+        });
         if (this.appendToBody) {
           document.body.appendChild(this.$el);
         }
-      }
-    },
-
-    destroyed() {
-      // if appendToBody is true, remove DOM node after destroy
-      if (this.appendToBody && this.$el && this.$el.parentNode) {
-        this.$el.parentNode.removeChild(this.$el);
+      } else {
+        this.$el.removeEventListener('scroll', this.updatePopper);
+        if (!this.closed) this.$emit('close');
+        if (this.destroyOnClose) {
+          this.$nextTick(() => {
+            this.key++;
+          });
+        }
       }
     }
-  };
+  },
+
+  mounted() {
+    if (this.visible) {
+      this.rendered = true;
+      this.open();
+      if (this.appendToBody) {
+        document.body.appendChild(this.$el);
+      }
+    }
+  },
+
+  destroyed() {
+    // if appendToBody is true, remove DOM node after destroy
+    if (this.appendToBody && this.$el && this.$el.parentNode) {
+      this.$el.parentNode.removeChild(this.$el);
+    }
+  },
+
+  methods: {
+    getMigratingConfig() {
+      return {
+        props: {
+          'size': 'size is removed.'
+        }
+      };
+    },
+    handleWrapperClick() {
+      if (!this.closeOnClickModal) return;
+      this.handleClose();
+    },
+    handleClose() {
+      if (typeof this.beforeClose === 'function') {
+        this.beforeClose(this.hide);
+      } else {
+        this.hide();
+      }
+    },
+    hide(cancel) {
+      if (cancel !== false) {
+        this.$emit('update:visible', false);
+        this.$emit('close');
+        this.closed = true;
+      }
+    },
+    updatePopper() {
+      this.broadcast('ElSelectDropdown', 'updatePopper');
+      this.broadcast('ElDropdownMenu', 'updatePopper');
+    },
+    afterEnter() {
+      this.$emit('opened');
+    },
+    afterLeave() {
+      this.$emit('closed');
+    }
+  }
+};
 </script>
